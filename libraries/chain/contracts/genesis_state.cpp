56fc0ff38d71c9be1c0576ccffce41753ad6edba
kwuaint <king_lear@tom.com>
2018-06-15 11:54:49 +0800

    update the genesis_state

--- libraries/chain/contracts/genesis_state.cpp
+++ libraries/chain/contracts/genesis_state.cpp
@@ -3,16 +3,16 @@
  *  @copyright defined in BithumbCoin/LICENSE.txt
  */
 
-#include <BithumbCoinio/chain/contracts/genesis_state.hpp>
+#include <bthbio/chain/contracts/genesis_state.hpp>
 
 // these are required to serialize a genesis_state
 #include <fc/smart_ref_impl.hpp>   // required for gcc in release mode
 
-namespace BithumbCoinio { namespace chain { namespace contracts {
+namespace bthbio { namespace chain { namespace contracts {
 
 
 chain::chain_id_type genesis_state_type::compute_chain_id() const {
    return initial_chain_id;
 }
 
-} } } // namespace BithumbCoinio::chain::contracts
+} } } // namespace bthbio::chain::contracts
