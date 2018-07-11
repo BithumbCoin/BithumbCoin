81c4f4847e7c4bd1cdfda69855da9614c1eaee02
kwuaint <king_lear@tom.com>
2018-07-11 10:51:05 +0800

    update internal_exceptions.hpp

--- libraries/chain/include/bthbio/chain/internal_exceptions.hpp
+++ libraries/chain/include/bthbio/chain/internal_exceptions.hpp
@@ -1,25 +1,25 @@
 /**
  *  @file
- *  @copyright defined in BithumbCoin/LICENSE.txt
+ *  @copyright defined in bthb/LICENSE.txt
  */
 #pragma once
 
 #include <fc/exception/exception.hpp>
-#include <BithumbCoinio/chain/exceptions.hpp>
+#include <bthbio/chain/exceptions.hpp>
 
-#define BithumbCoin_DECLARE_INTERNAL_EXCEPTION( exc_name, seqnum, msg )  \
+#define BTHB_DECLARE_INTERNAL_EXCEPTION( exc_name, seqnum, msg )  \
    FC_DECLARE_DERIVED_EXCEPTION(                                      \
       internal_ ## exc_name,                                          \
-      BithumbCoinio::chain::internal_exception,                            \
+      bthbio::chain::internal_exception,                            \
       3990000 + seqnum,                                               \
       msg                                                             \
       )
 
-namespace BithumbCoinio { namespace chain {
+namespace bthbio { namespace chain {
 
-FC_DECLARE_DERIVED_EXCEPTION( internal_exception, BithumbCoinio::chain::chain_exception, 3990000, "internal exception" )
+FC_DECLARE_DERIVED_EXCEPTION( internal_exception, bthbio::chain::chain_exception, 3990000, "internal exception" )
 
-BithumbCoin_DECLARE_INTERNAL_EXCEPTION( verify_auth_max_auth_exceeded, 1, "Exceeds max authority fan-out" )
-BithumbCoin_DECLARE_INTERNAL_EXCEPTION( verify_auth_account_not_found, 2, "Auth account not found" )
+BTHB_DECLARE_INTERNAL_EXCEPTION( verify_auth_max_auth_exceeded, 1, "Exceeds max authority fan-out" )
+BTHB_DECLARE_INTERNAL_EXCEPTION( verify_auth_account_not_found, 2, "Auth account not found" )
 
-} } // BithumbCoinio::chain
+} } // bthbio::chain
